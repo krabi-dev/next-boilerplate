9d7d7fad31838c3c256647d8495ebfadc4852ec1
Zenkilies Vildcent <zenkilies@gmail.com>
2020-11-13 15:50:33 +0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <p>
+      Hello, world!
+    </p>
+  );
+}
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/**/*.{jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
